801bac27b614fdfa397c9440e4e401699dceca57
fhtgrjlirtjgv <154062090+fhtgrjlirtjgv@users.noreply.github.com>
2024-02-18 17:32:46 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,19 +1,37 @@
-from PyQt5.QtWidgets import QApplication
+import json
 from PyQt5.QtWidgets import QMainWindow
 from ui import Ui_MainWindow
+from PyQt5.QtWidgets import QApplication
 
 class Widget(QMainWindow):
     def   __init__(self):
         super().__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.ui.setupUi(self)
         self.notes = {
-            "Перша амітка":{
-                "текст": "Це текст першої амітки",
+            "Перша замітка":{
+                "текст": "Це текст першої замітки",
                 "теги":[]
             }
         }
-        self.ui.notes_list.addItems(self.notes)
+        self.ui.notate_bibliot.addItems(self.notes)
+        self.ui.notate_bibliot.itemClicked.connect(self.show_note)
+        self.ui.save_notate.clicked.connect(self.save_note)
+    def show_note(self):
+        name = self.ui.notate_bibliot.selectedItems()[0].text()
+        self.ui.line_ss.setText(name)
+        self.ui.text_Edit_big.setText(self.notes[name]["текст"])
+
+    def save_note(self):
+        self.notes[self.ui.line_ss.text()] = self.ui.text_Edit_big.toPlainText()
+        with open("notes.json", "w", encoding="utf-8" ) as file:
+            json.dump(self.notes, file)
+
+
+
+
+
 
 app = QApplication([])
 ex = Widget()
--- ui.py
+++ ui.py
@@ -14,7 +14,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(653, 540)
+        MainWindow.resize(1009, 794)
         MainWindow.setStyleSheet("background:#bebebe ;")
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
